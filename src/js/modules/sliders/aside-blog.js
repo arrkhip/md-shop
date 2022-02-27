bfb4d1fbeb03975bb4837ff378c396953f147ddc
Alex Sheremetev <alexsher55@yandex.ru>
2022-02-27 12:18:39 +0600

    add block 'subscribe'

--- src/js/modules/sliders/aside-blog.js
+++ src/js/modules/sliders/aside-blog.js
@@ -1,18 +1,17 @@
 import Swiper from 'swiper/swiper-bundle.min';
 
-const asideSlider = document.querySelector('.aside-blog');
-let slider;
+const $sliders = document.querySelectorAll('.aside-blog__slider');
 
-if (asideSlider) {
-  slider = new Swiper('.aside-blog .swiper', {
-    spaceBetween: 20,
-    speed: 500,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
+if ($sliders.length) {
+  $sliders.forEach(($slider) => {
+    const slider = new Swiper($slider, {
+      spaceBetween: 20,
+      speed: 500,
+      pagination: {
+        type: 'bullets',
+        el: '.swiper-pagination2',
+        clickable: true,
+      },
+    });
   });
 }
-
-if (!window.sliders) window.sliders = {};
-if (slider) window.sliders.asideSlider = slider;
